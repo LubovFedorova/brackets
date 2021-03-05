9d57aa5e77505a75fa561cdf5e716f0877f00663
Lubov Fedorova <luba.zaverugo@gmail.com>
2021-03-05 09:10:27 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let current of str) {
+    if (isOpenBracket(current,bracketsConfig)) {
+      stack.push(current);
+    }
+    if (isClosedBracket(current,bracketsConfig)) {
+      if(stack.length===0){
+        return false;
+      }
+      let prev = stack.pop();
+      if (isPairBracket(prev, current,bracketsConfig)) {
+        continue;
+      }
+      return false
+    }
+  }
+  if (stack.length === 0) {
+    return true
+  }
+  return false
 }
+
+
+function isOpenBracket(symbol,config) {
+  for (const brackets of config) {
+      let open = brackets[0];
+      if (symbol === open)  {
+        return true
+      }  
+  }
+  return false
+}
+
+function isClosedBracket(symbol,config) {
+  for (const brackets of config) {
+    let close = brackets[1];
+    if (symbol === close) {
+      return true
+    }
+  }
+  return false
+}
+
+function isPairBracket (openSymbol, closeSymbol,config) {
+  for (const brackets of config) {
+    let open = brackets[0];
+    let close = brackets[1];
+    if (openSymbol === open && closeSymbol === close) {
+      return true
+    }
+
+  }
+  return false
+}
+
